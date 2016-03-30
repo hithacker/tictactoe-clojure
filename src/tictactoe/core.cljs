8f75df57820c678ffe641bcb967abeb3510d7ce1
hithacker <hithacker@gmail.com>
2016-03-30 16:35:00 +0530

    added checking for column

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -8,13 +8,23 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defn new-board [n]
-  (vec (repeat n [0 0 0])))
-
-(prn (new-board 3))
+  (vec (repeat n [nil nil nil])))
 
 (defonce app-state
   (atom {:text "Welcome to Tic Tac Toe"
-         :board (new-board 3)}))
+         :board (new-board 3)
+         :turn :p1}))
+
+(defn next-turn [turn]
+  (if (= turn :p1)
+    :p2
+    :p1))
+
+(defn win? [turn]
+  (let [board (:board @app-state)]
+    (if (some true? (map (fn [seq] (every? #(= % turn) seq)) board))
+      true
+      false)))
 
 (defn tictactoe []
   [:div
@@ -26,18 +36,32 @@
       :height 500
       :style {:border "1px solid #cccccc"}}]
     (for [i (range (count (:board @app-state)))
-          j (range (count (:board @app-state)))]
-      [:rect {:x i
-              :y j
-              :width 0.9
-              :height 0.9
-              :fill "green"
-              :on-click
-              (fn rect-click [e]
-                (prn "You clicked me!" i j)
-                (prn
-                 (swap! app-state update-in [:board i j] inc)))}]))])
-
+          j (range 3)]
+      (let [cell-value (get-in @app-state [:board i j])]
+        [:rect {:x i
+                :y j
+                :width 0.99
+                :height 0.99
+                :fill (cond
+                        (= :p1 cell-value) "yellow"
+                        (= :p2 cell-value) "red"
+                        :else "green")
+                :on-click
+                (fn rect-click [e]
+                  (let [turn (:turn @app-state)]
+                    (if (nil? cell-value)
+                      (do
+                        (swap! app-state
+                               assoc
+                               :turn
+                               (next-turn turn))
+                        (swap! app-state
+                               assoc-in
+                               [:board i j]
+                               (next-turn turn))
+                        (if (win? (next-turn turn))
+                          (js/alert (str (turn) " wins")))))))}])))])
+  
 (reagent/render-component [tictactoe]
                           (. js/document (getElementById "app")))
 
